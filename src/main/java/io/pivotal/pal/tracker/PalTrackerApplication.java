445c063d766ccc3f965597c5716f2394009281bf
Kesavan Lakshmanan <kesavan.lakshmanan@cognizant.com>
2020-03-24 03:29:06 -0500

    Update EnvController, PalTracker,WelcomController

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,4 +1,20 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+
+
 public class PalTrackerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PalTrackerApplication.class,
+                args);
+    }
+
+    @GetMapping
+    public String message() {
+        return "I'm ready for PAL1";
+    }
 }
