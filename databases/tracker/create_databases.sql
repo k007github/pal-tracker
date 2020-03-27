1b23a640d8549b517a7d65e152de447a881cc812
Kesavan Lakshmanan <kesavan.lakshmanan@cognizant.com>
2020-03-27 04:10:35 -0500

    Pipeline update

--- databases/tracker/create_databases.sql
+++ databases/tracker/create_databases.sql
@@ -5,6 +5,6 @@ CREATE DATABASE tracker_dev;
 CREATE DATABASE tracker_test;
 
 CREATE USER IF NOT EXISTS 'tracker'@'%'
-  IDENTIFIED BY 'tracker';
+  IDENTIFIED BY '';
 GRANT ALL PRIVILEGES ON tracker_dev.* TO 'tracker' @'%';
 GRANT ALL PRIVILEGES ON tracker_test.* TO 'tracker' @'%';
